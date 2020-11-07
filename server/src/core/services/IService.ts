2d1e92ecee3dab389d5092d2028e95350448f40a
Hacktin <isues41@gmail.com>
2020-11-07 18:28:17 +0300

    base service's generic has became to base model

--- server/src/core/services/IService.ts
+++ server/src/core/services/IService.ts
@@ -1,16 +1,17 @@
-import IEntity from "../entities/IEntity";
+
+import BaseModel from "../models/BaseModel";
 import ServiceResponse from "./ServiceResponse";
 
 
-export default interface IService<T extends IEntity>{
+export default interface IService<T extends BaseModel>{
 
     GetAll():Promise<ServiceResponse<T>>
 
     GetById(id:number):Promise<ServiceResponse<T>>
 
-    Add(entity:T):Promise<ServiceResponse<T>>
+    Add(model:T):Promise<ServiceResponse<T>>
 
     Delete(id:number):Promise<ServiceResponse<T>>
 
-    Update(id:number,entity:T):Promise<ServiceResponse<T>>
+    Update(id:number,model:T):Promise<ServiceResponse<T>>
 }
