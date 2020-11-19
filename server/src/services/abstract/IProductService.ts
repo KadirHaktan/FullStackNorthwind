e12a09c192595b2bfa1c06f477664d7f3ae76ba5
Hacktin <isues41@gmail.com>
2020-11-19 12:19:55 +0300

    new two signature methods has been defined.But these methods implemantation has not done yet

--- server/src/services/abstract/IProductService.ts
+++ server/src/services/abstract/IProductService.ts
@@ -1,8 +1,13 @@
 import { BaseProductModel } from "../../models/abstract/BaseProductModel";
 import IService from "../../core/services/IService";
+import ServiceResponse from "src/core/services/ServiceResponse";
+import { ProductCategoryModel } from "src/models/concerete/productModels/ProductCategoryModel";
 
 
 
 export default interface IProductService<T extends BaseProductModel> extends IService<T>{
 
+    GetProductsWithCategoryInfos():ServiceResponse<ProductCategoryModel> 
+
+    GetProductWithCategoryInfo(id:number):ServiceResponse<ProductCategoryModel>
 }
